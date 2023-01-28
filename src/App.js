cda4dfb7865b97c6dd6e4b249ec9ab299d52a1df
Brask <Dafribdfv@gmail.com>
2023-01-28 02:18:51 -0500

    Version base: Contador de clics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import Contador from './componentes/contador';
+import Boton from './componentes/Boton';
+import logoBrask from './imagenes/logo-brask.jpg'
+import { useState } from 'react';
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='brask-logo-contenedor'>
+        <img 
+        className='brask-logo'
+        src={logoBrask} 
+        alt='Logo Brask' />
+      </div>
+      <div className='contenedor-principal'>
+        <Contador numClics={numClics} />
+        <Boton 
+          texto='clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+
+      </div>
     </div>
   );
 }
